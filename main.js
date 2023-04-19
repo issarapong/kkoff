5fadbcf6b4a051c402067f93ac4897e423366790
IS <mr.issarapong@gmail.com>
2023-04-19 11:25:25 +0700

    เก็บ Dom ทั้งหมดใส่ตัวแปร

---
+++ main.js
@@ -0,0 +1,17 @@
+//SelectDOM:input เก็บเข้าตัวแปร
+
+const priceInput = document.getElementById('product-price');
+const quantityInput =document.getElementById('product-quantity');
+const shippingInput = document.getElementById('product-shipping');
+
+//Select DOM:show Resut
+const errorBox = document.getElementById('error');
+const resultBox = document.getElementById('result');
+
+//Select DOM:Event Handler
+const totalBtn = document.getElementById('total-btn');
+
+//logcheck
+console.log(priceInput);
+
+
