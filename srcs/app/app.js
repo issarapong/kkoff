955aa31a00ad25d817af87daf7417a9aeb34df99
IS <mr.issarapong@gmail.com>
2023-04-19 14:50:56 +0700

    complete refactor code

---
+++ srcs/app/app.js
@@ -0,0 +1,28 @@
+// const q = document.getElementById('error');
+// console.log(q);
+
+const app = () => {
+    const componentService = new ComponentService();
+    const errorService = new ErrorService();
+
+    errorService.hideError(); //*** */
+
+    const calTotal = () => {
+        errorService.hideError();
+        const inputs = componentService.getInputs(); //*** */ ได้ array ของ input
+        const numbers = parseInput(...inputs); // [1,2,4] , ...[1,2,4] == 1,2,4
+        const valid = validateInputs(...numbers);
+
+        if (valid) {
+            const [price, quantity, shipping] = numbers;
+            const totalPrice = price * quantity + shipping;
+            componentService.showPrice(totalPrice); // Display ราคา
+        } else {
+            errorService.showErrorMessage(inputs, numbers); //*** */
+        }
+    };
+
+    componentService.onClick(calTotal); //*** */
+};
+
+app();
